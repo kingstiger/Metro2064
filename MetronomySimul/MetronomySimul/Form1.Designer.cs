512f04bb8003dd7eea4db5828f12aa67fa17d703
NiNJAxFREEZu <frugoxmaniak@gmail.com>
2019-05-08 17:24:42 +0200

    Dodanie bardzo, bardzo wstępnej implementacji klasy NetInterface

--- MetronomySimul/MetronomySimul/Form1.Designer.cs
+++ MetronomySimul/MetronomySimul/Form1.Designer.cs
@@ -28,21 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.splitter1 = new System.Windows.Forms.Splitter();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.tabControl1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Location = new System.Drawing.Point(12, 12);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(954, 606);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(946, 580);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Parametry symulacji";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(946, 580);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Aktywne połączenia";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// splitter1
+			// 
+			this.splitter1.Location = new System.Drawing.Point(0, 0);
+			this.splitter1.Name = "splitter1";
+			this.splitter1.Size = new System.Drawing.Size(8, 630);
+			this.splitter1.TabIndex = 1;
+			this.splitter1.TabStop = false;
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Cursor = System.Windows.Forms.Cursors.No;
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(946, 580);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "Log";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(978, 630);
+			this.Controls.Add(this.splitter1);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "Form1";
+			this.Text = "Drgające metronomy";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tabControl1.ResumeLayout(false);
+			this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.Splitter splitter1;
+	}
 }
 
--- MetronomySimul/MetronomySimul/NetInterface.cs
+++ MetronomySimul/MetronomySimul/NetInterface.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
 
 /*
 Klasa NetInterface jest implementacją interfejsu sieciowego metronomu. Klasa ta posiada swoją instancję klienta UDP, oraz metody
@@ -13,5 +11,21 @@ namespace MetronomySimul
 {
 	class NetInterface
 	{
+		UdpClient client;											//Instancja klasy UdpClient do przesyłania danych przez sieć za pomocą protokołu UDP
+		IPEndPoint localEndPoint, targetEndPoint;					//Instancje klasy IPEndPoint zawierają pary adres IPv4 oraz numer portu endpointu nadawcy i odbiorcy
+		Queue<NetPacket> packetsToSend, packetsReceived;			//Kolejki (bufory) komunikatów przychodzących i oczekujących na wysłanie
+
+		NetInterface()
+		{
+			packetsToSend = new Queue<NetPacket>();					//Inicjalizacja buforów na komunikaty
+			packetsReceived = new Queue<NetPacket>();
+
+			localEndPoint = new IPEndPoint(IPAddress.Any, 0);		//Lokalny endpoint otrzyma adres karty sieciowej i wolny numer portu
+			targetEndPoint = new IPEndPoint(IPAddress.None, 0);		//Na początku nie znamy danych nadawcy, dlatego w konstruktorze przypisujemy pusty adres IPv4
+
+			//localEndPoint.Address = IPAddress.Parse("192.168.0.1");
+
+			
+		}
 	}
 }
