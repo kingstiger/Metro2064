b84954951d768f4f4463e2b2fa46b02124fe6c13
NiNJAxFREEZu <frugoxmaniak@gmail.com>
2019-05-15 20:39:34 +0200

    Rozwinięcie dokumentacji protokołu Net

--- MetronomySimul/MetronomySimul/NetInterface.cs
+++ MetronomySimul/MetronomySimul/NetInterface.cs
@@ -11,21 +11,24 @@ namespace MetronomySimul
 {
 	class NetInterface
 	{
-		UdpClient client;											//Instancja klasy UdpClient do przesyłania danych przez sieć za pomocą protokołu UDP
-		IPEndPoint localEndPoint, targetEndPoint;					//Instancje klasy IPEndPoint zawierają pary adres IPv4 oraz numer portu endpointu nadawcy i odbiorcy
-		Queue<NetPacket> packetsToSend, packetsReceived;			//Kolejki (bufory) komunikatów przychodzących i oczekujących na wysłanie
+		UdpClient client;                                           //Instancja klasy UdpClient do przesyłania danych przez sieć za pomocą protokołu UDP
+		IPEndPoint localEndPoint, targetEndPoint;                   //Instancje klasy IPEndPoint zawierają pary adres IPv4 oraz numer portu endpointu nadawcy i odbiorcy
+		Queue<NetPacket> packetsToSend, packetsReceived;            //Kolejki (bufory) komunikatów przychodzących i oczekujących na wysłanie
 
-		NetInterface()
+		public NetInterface(int interfaceNumber)                    //interfaceNumber oznacza numer interfejsu w celu dobrania odpowiedniego portu
 		{
-			packetsToSend = new Queue<NetPacket>();					//Inicjalizacja buforów na komunikaty
+			packetsToSend = new Queue<NetPacket>();                 //Inicjalizacja buforów na komunikaty
 			packetsReceived = new Queue<NetPacket>();
 
-			localEndPoint = new IPEndPoint(IPAddress.Any, 0);		//Lokalny endpoint otrzyma adres karty sieciowej i wolny numer portu
-			targetEndPoint = new IPEndPoint(IPAddress.None, 0);		//Na początku nie znamy danych nadawcy, dlatego w konstruktorze przypisujemy pusty adres IPv4
+			localEndPoint = new IPEndPoint(IPAddress.Any, GetPortNumber(interfaceNumber));      //Lokalny endpoint otrzyma adres karty sieciowej i wolny numer portu
+			targetEndPoint = new IPEndPoint(IPAddress.None, 0);                                 //Na początku nie znamy danych nadawcy, dlatego w konstruktorze przypisujemy pusty adres IPv4
 
-			//localEndPoint.Address = IPAddress.Parse("192.168.0.1");
+			client = new UdpClient(localEndPoint);                  //Inicjalizacja klienta protokołu UDP
 
-			
+
 		}
+
+		//Metoda GetPortNumber() zwraca numer portu na podstawie numeru interfejsu sieciowego LAMBDY SĄ FAJNE xD
+		private int GetPortNumber(int interfaceNumber) => 8080 + interfaceNumber;
 	}
 }
