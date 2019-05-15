926a393b869ac9fc86fcc043f2c081fe10515973
NiNJAxFREEZu <frugoxmaniak@gmail.com>
2019-05-15 22:35:21 +0200

    Część implementacji klasy NetInterface

--- MetronomySimul/MetronomySimul/NetInterface.cs
+++ MetronomySimul/MetronomySimul/NetInterface.cs
@@ -2,7 +2,7 @@
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
-
+using System.Threading.Tasks;
 /*
 Klasa NetInterface jest implementacją interfejsu sieciowego metronomu. Klasa ta posiada swoją instancję klienta UDP, oraz metody
 pozwalające na komunikację z innymi interfejsami. Schemat datagramu wykorzystywanego przez tą klasę znajduję się w klasie NetPacket.
@@ -15,9 +15,8 @@ namespace MetronomySimul
 		private UdpClient client;                                           //Instancja klasy UdpClient do przesyłania danych przez sieć za pomocą protokołu UDP
 		private IPEndPoint localEndPoint, targetEndPoint;                   //Instancje klasy IPEndPoint zawierają pary adres IPv4 oraz numer portu endpointu nadawcy i odbiorcy
 		private Queue<NetPacket> packetsToSend, packetsReceived;            //Kolejki (bufory) komunikatów przychodzących i oczekujących na wysłanie
-		private bool isAvailable;                                           //Oznacza, czy interfejs nie ma już połączenia
-		Thread senderThread;												//Uchwyty na wątki
-		Thread listenerThread;
+		private bool isAvailable { get; set; }                              //Oznacza, czy interfejs nie ma już połączenia
+		Thread senderThread, listenerThread, processingThread;				//Uchwyty na wątki
 
 		public NetInterface(int interfaceNumber)                    //interfaceNumber oznacza numer interfejsu w celu dobrania odpowiedniego portu
 		{
@@ -26,25 +25,32 @@ namespace MetronomySimul
 			packetsReceived = new Queue<NetPacket>();
 			senderThread = new Thread(new ThreadStart(SenderThread));
 			listenerThread = new Thread(new ThreadStart(ListenerThread));
+			processingThread = new Thread(new ThreadStart(ProcessingThread));
 
 			localEndPoint = new IPEndPoint(IPAddress.Any, GetPortNumber(interfaceNumber));      //Lokalny endpoint otrzyma adres karty sieciowej i wolny numer portu
-			client = new UdpClient(localEndPoint);                  //Inicjalizacja klienta protokołu UDP
+			client = new UdpClient(localEndPoint);												//Inicjalizacja klienta protokołu UDP
 		}
 
 		private int GetPortNumber(int interfaceNumber) => 8080 + interfaceNumber;
 
-		private bool ImAvailable() => this.isAvailable = true;
-
-		private bool ImNotAvailable() => this.isAvailable = false;
-
 		private void ListenerThread()   //Wątek odbierający komunikaty z sieci
 		{
-
+			while(true)
+			{
+				//odbieranie pakietu
+				//dodanie go do kolejki pakietów odebranych
+			}
 		}
 
 		private void SenderThread()     //Wątek wysyłający komunikaty w sieć
 		{
-
+			while(true)
+			{
+				if(packetsToSend.Count > 0)
+				{
+					//Wyślij pakiet z kolejki
+				}
+			}
 		}
 
 		public bool IsAvailable() => this.isAvailable;
@@ -59,7 +65,7 @@ namespace MetronomySimul
 			listenerThread.Start();
 
 			//Sorki, mam chłopaka
-			ImNotAvailable();
+			isAvailable = false;
 		}
 
 		public void TerminateConnection()
@@ -74,7 +80,12 @@ namespace MetronomySimul
 			targetEndPoint = null;
 
 			//Od dzisiaj jestem wolna
-			ImAvailable();
+			isAvailable = true;
+		}
+
+		private void ProcessingThread()     //Przetwarzanie pakietów, ahhhhhhh to bdzie duże
+		{
+			return;
 		}
 	}
 }
